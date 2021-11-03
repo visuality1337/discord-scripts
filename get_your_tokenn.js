38e0503473cdc4a41933993e12bd90b890f63009
Visual <68449061+Visual9999@users.noreply.github.com>
2021-11-03 21:13:56 +0100

    Create get_your_tokenn.js

---
+++ get_your_tokenn.js
@@ -0,0 +1,8 @@
+OLD THAT GOT PACTHED:
+
+Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getToken!==void 0).exports.default.getToken()
+
+
+NEW THAT I FIXED:
+
+Object.values(webpackChunkdiscord_app.push([[Math.random()], [], (e) => r=e.c])).find(m => m.exports && m.exports.default && m.exports.default.getToken).exports.default.getToken()
