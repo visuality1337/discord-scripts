55a80603c6c0b9d5f00e7cef2afac6dd2d447cf1
Visual <68449061+Visual9999@users.noreply.github.com>
2021-11-03 21:12:40 +0100

    Create get_every_bade.js

---
+++ get_every_bade.js
@@ -0,0 +1,8 @@
+OLD THAT GOT PATCHED:
+
+Object.values(webpackJsonp.push([[],{[''] :(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser!==void0).exports.default.getCurrentUser().flags=-33
+
+
+
+NEW THAT I FIXED:
+Object.values(webpackChunkdiscord_app.push([[Math.random()], [], (e) => r=e.c])).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser).exports.default.getCurrentUser().flags=-33
