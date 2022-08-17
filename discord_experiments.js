12b42d233e5cc78dbca72b2b1917c24eb48215f9
Visual <68449061+Visual9999@users.noreply.github.com>
2022-08-17 11:18:13 +0200

    old got patched but here is new one

--- discord_experiments.js
+++ discord_experiments.js
@@ -1,48 +1,12 @@
-OLD THAT GOT PATCHED:
-
-(() => {
-    const req = typeof(webpackJsonp) === "function" ? webpackJsonp([], {
-        '__extra_id__': (module, exports, req) => exports.default = req
-    }, ['__extra_id__']).default : webpackJsonp.push([[], {
-        '__extra_id__': (module, exports, req) => module.exports = req
-    }, [['__extra_id__']]]);
-    delete req.m['__extra_id__'];
-    delete req.c['__extra_id__'];
-    const find = (filter, options = {}) => {
-        const {cacheOnly = true} = options;
-        for (let i in req.c) {
-            if (req.c.hasOwnProperty(i)) {
-                let m = req.c[i].exports;
-                if (m && m.__esModule && m.default && filter(m.default))
-                    return m.default;
-                if (m && filter(m))
-                    return m;
-            }
-        }
-        if (cacheOnly) {
-            console.warn('Cannot find loaded module in cache');
-            return null;
-        }
-        console.warn('Cannot find loaded module in cache. Loading all modules may have unexpected side effects');
-        for (let i = 0; i < req.m.length; ++i) {
-            try {
-                let m = req(i);
-                if (m && m.__esModule && m.default && filter(m.default))
-                    return m.default;
-                if (m && filter(m))
-                    return m;
-            }
-            catch (e) {
-            }
-        }
-        console.warn('Cannot find module');
-        return null;
-    };
-    const findByUniqueProperties = (propNames, options) => find(module => propNames.every(prop => module[prop] !== undefined), options);
-    Object.defineProperty(findByUniqueProperties(["isDeveloper"]),"isDeveloper",{get:_=>1,set:_=>_,configurable:true});
-})();
-
-
-NEW ONE BY ME WITH JUST ONE LINE:
-
-Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true}); 
+let wpRequire;
+window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.default?.isDeveloper !== "undefined")
+usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+nodes = Object.values(mod.exports.default._dispatcher._actionHandlers._dependencyGraph.nodes)
+try {
+    nodes.find(x => x.name == "ExperimentStore").actionHandler["CONNECTION_OPEN"]({user: {flags: 1}, type: "CONNECTION_OPEN"})
+} catch (e) {}
+oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+usermod.exports.default.__proto__.getCurrentUser = () => ({hasFlag: () => true})
+nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+usermod.exports.default.__proto__.getCurrentUser = oldGetUser
